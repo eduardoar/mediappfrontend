389310bda07ca2a3d892f9e5f1fbb9fa9f599eb5
Eduardo Anchiraico <edu0793@gmail.com>
2021-11-02 07:19:48 -0300

    Adicionando CRUD para examenes y cardiologia

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,25 @@ import { RouterModule, Routes } from '@angular/router';
 import { PacienteComponent } from './pages/paciente/paciente.component';
 import { MedicoComponent } from './pages/medico/medico.component';
 import { PacienteEdicionComponent } from './pages/paciente/paciente-edicion/paciente-edicion.component';
+import { ExamenComponent } from './pages/examen/examen.component';
+import { EspecialidadComponent } from './pages/especialidad/especialidad.component';
+import { ExamenEdicionComponent } from './pages/examen/examen-edicion/examen-edicion.component';
+import { EspecialidadEdicionComponent } from './pages/especialidad/especialidad-edicion/especialidad-edicion.component';
 
 const routes: Routes = [
   {path: 'paciente', component: PacienteComponent, children: [
     {path: 'nuevo', component: PacienteEdicionComponent},
     {path: 'edicion/:id', component: PacienteEdicionComponent}
   ]},
-  {path: 'medico', component: MedicoComponent}
+  {path: 'medico', component: MedicoComponent},
+  {path: 'examen', component: ExamenComponent, children: [
+    {path: 'nuevo', component: ExamenEdicionComponent},
+    {path: 'edicion/:id', component: ExamenEdicionComponent}
+  ]},
+  {path: 'especialidad', component: EspecialidadComponent, children: [
+    {path: 'nuevo', component: EspecialidadEdicionComponent},
+    {path: 'edicion/:id', component: EspecialidadEdicionComponent}
+  ]},
 ];
 
 @NgModule({
